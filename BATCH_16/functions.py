e4cc25cab40b94f7f128f67a801d906a3997bb23
Vijay <be1newinner@gmail.com>
2025-02-21 15:53:14 +0530

    Taught : functions

---
+++ BATCH_16/functions.py
@@ -0,0 +1,40 @@
+### functions = def
+
+''' 
+Simple type of function
+def xyz():
+   print("Vijay")
+   print("Vijay2")
+   print("Vijay3")
+   print("Vijay4")
+   print("Vijay5")
+   print("Vijay6")
+'''
+
+
+'''
+function with arguments / dynamic functions
+
+def fullname(fname, lname, age):
+    name = fname + " " + lname
+    print("I am" , name)
+    print("My age is ", age)
+
+
+
+fullname("Vijay","Kumar", 25)
+fullname("Ajay","Kumar", 24)
+fullname("Karan","Kumar", 21)
+
+'''
+
+
+### Optional Arguments
+def fullname(fname, lname, age=25):
+    name = fname + " " + lname
+    print("I am" , name)
+    print("My age is ", age)
+
+fullname("Vijay","Kumar", 25)
+fullname("Ajay","Kumar")
+fullname("Karan","Kumar", 21)
