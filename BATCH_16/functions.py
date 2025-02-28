7f865c1affb19143edbe6c5e2f96990f8d48322c
Vijay <be1newinner@gmail.com>
2025-02-28 15:24:26 +0530

    Taught Recursive and Lambda of function

--- BATCH_16/functions.py
+++ BATCH_16/functions.py
@@ -44,8 +44,6 @@ fullname("Karan","Kumar", 21)
 ''' 
 Return Values
 
-'''
-
 def sum(a,b):
     total = a + b
     return total
@@ -56,3 +54,36 @@ def sum(a,b):
 print(sum(7,6))
 #print(13)
 
+
+# lambda arguments : expression
+
+def sum(a,b):
+   return a + b
+   
+result = sum(5,6)
+
+print(result)
+
+sum = lambda a,b : a + b
+
+result = sum(5,6)
+
+print(result)
+
+'''
+
+# Recursive function
+
+# Find factorial of a number
+# e.g. 5 => 5 * 4 * 3 * 2 * 1
+
+def fact(n):
+    if( n > 0 ):
+        return n * fact( n - 1)
+    else: 
+        return 1
+    
+result = fact(5)
+print(result)
+
+# return 5 * 4 * 3 * 2 * 1 * 0 * -1
