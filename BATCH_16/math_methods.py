a1ae3cee0402e12d88f8712fe129fe8ce731a00d
Vijay <be1newinner@gmail.com>
2025-03-26 15:14:10 +0530

    tAUGHT maTH mODULE

---
+++ BATCH_16/math_methods.py
@@ -0,0 +1,42 @@
+'''
+math.ceil(x)	Rounds up to the nearest integer
+math.floor(x)	Rounds down to the nearest integer
+math.trunc(x)	Truncates the decimal part, keeping only the integer part
+math.fabs(x)	Absolute value of x (float)
+math.factorial(x)	Returns x! (factorial of x)
+math.gcd(x, y)	Returns the greatest common divisor (GCD) of x and y
+math.lcm(x, y)	Returns the least common multiple (Python 3.9+)
+math.isfinite(x)	Checks if x is a finite number
+math.isinf(x)	Checks if x is infinity
+math.isnan(x)	Checks if x is NaN (Not a Number)
+math.exp(x)	Returns e^x
+math.expm1(x)	Returns e^x - 1 (more precise for small x)
+math.log(x,[base])	Returns the logarithm of x to the given base (default is e)
+math.log2(x)	Returns the base-2 logarithm of x
+math.log10(x)	Returns the base-10 logarithm of x
+math.pow(x, y)	Returns x^y (power)
+math.sqrt(x)	Returns the square root of x
+''' 
+
+import math
+
+float_num = 2.54
+num = math.inf
+num2 = math.nan
+
+# print(math.log(10,10))
+# print(math.ceil(0.45))
+# print(math.floor(0.45))
+# print(math.trunc(float_num))
+# print(math.fabs(2))
+# print(float_num - math.trunc(float_num))
+# print(math.factorial(5)) # 5 * 4* 3* 2* 1
+# print(math.gcd(20, 16)) 
+# print(math.lcm(20, 16)) 
+# print(math.isfinite(num)) 
+# print(num) 
+# print(math.isnan(math.nan))
+# print(math.exp(2))
+# print(math.log10(10))
+# print(math.pow(4,5), 4**5)
+# print(math.sqrt(16))
