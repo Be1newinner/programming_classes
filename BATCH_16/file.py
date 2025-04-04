71f23c2088d9462e45e2789c1bc8ad4f4ed42cf4
Vijay <be1newinner@gmail.com>
2025-04-04 15:25:05 +0530

    file and os module

---
+++ BATCH_16/file.py
@@ -0,0 +1,30 @@
+# f = open("dicta.py", "a") # r, w, a, x
+import os
+
+# ================== Read Operations
+
+# dat = f.read(100)
+# dat = f.readline(100)
+# dat = f.readline()
+# data = f.readlines()
+# print(data[2])
+# print(data)
+
+# ================== Write Operations
+
+# f.write("print('helloaaaaa')")
+# f.write("\nprint('helloaaaaa')")
+
+# ================= os Module
+
+# os.remove("dicta.py")
+
+# os.mkdir("something")
+
+# os.rmdir("something") # remove a folder
+
+# os.rename("dict.py", "dicta.py")
+
+# print(os.listdir("./"))
+
+# print(os.path.exists("dict.py"))
---
+++ node/fs.js
@@ -0,0 +1,23 @@
+const fs = require("fs");
+
+// ##### File Operations
+//
+// fs.readFile()
+// fs.readFileSync()
+// fs.writeFileSync()
+// fs.writeFile()
+// fs.appendFile()
+// fs.appendFileSync()
+//
+
+// #### Directory Operations
+//
+// fs.mkdir()
+// fs.mkdirSync()
+// fs.readdirSync()
+// fs.readdir()
+// fs.rm()
+// fs.rmSync()
+// fs.stat()
+// fs.statSync()
+//
