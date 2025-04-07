ba5093fa0864cfade6faa5d7a029333b2fb1ed68
Vijay <be1newinner@gmail.com>
2025-04-07 15:29:20 +0530

    Add MongoDB setup with initial configuration and .gitignore

---
+++ BATCH_14/backend/mongodb/index.js
@@ -0,0 +1,19 @@
+// MONGODB SERVER = LOCAL ( CLUSTER )
+// MULTIPLE DATABSE, every company will have its own database
+//  => Multiple collections = folders
+// ==> multiple documnets = entries
+
+/*
+e.g. ecommerce = database
+
+collections ( array format ) => products, categories, users, orders, carts
+
+documents = ( object format )
+products collections = 
+1. {
+ "title": "Apple iphone",
+  "price" : 8000
+}
+
+
+*/
