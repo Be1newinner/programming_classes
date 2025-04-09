60b72f67c2b208f78a0ba2ef01d44c4f25a83653
Vijay <be1newinner@gmail.com>
2025-04-09 15:24:40 +0530

    Refactor class definitions and comments in classes.py for clarity and organization

--- BATCH_16/classes.py
+++ BATCH_16/classes.py
@@ -37,17 +37,61 @@ print(acer_laptop3.warranty)
 
 '''
 
-class Laptop:    
-    model = ""
-    warranty = 0
+# class Laptop:    
+#     model = ""
+#     warranty = 0
     
-    def __init__(self, model_value, w): # Constructor
-    #    print("CONSTRUCTOR")
-        self.model = model_value
-        self.warranty = w
+#     def __init__(self, model_value, w): # Constructor
+#     #    print("CONSTRUCTOR")
+#         self.model = model_value
+#         self.warranty = w
 
-acer_laptop = Laptop("ACER", 5)
-asus_laptop = Laptop("ASUS", 10)
+# acer_laptop = Laptop("ACER", 5)
+# asus_laptop = Laptop("ASUS", 10)
 
-print(acer_laptop.model, acer_laptop.warranty)
-print(asus_laptop.model, asus_laptop.warranty)
+# print(acer_laptop.model, acer_laptop.warranty)
+# print(asus_laptop.model, asus_laptop.warranty)
+
+
+# class Laptop:    
+#     model = ""
+#     warranty = 0
+    
+#     def __init__(self, model_value, w): # Constructor
+#     #    print("CONSTRUCTOR")
+#         self.model = model_value
+#         self.warranty = w
+        
+#     def calc(self, yr):
+#         self.warranty += yr
+#         print(self.warranty)
+
+# acer_laptop = Laptop("ACER", 5)
+# acer_laptop.calc(5)
+
+
+class Cat:
+    name = ""
+    
+    def  __init__(self, pet_name):
+        self.name = pet_name
+    
+    def speak(self):
+        print("My pet name is",self.name)
+
+# class Dog(Cat):
+#     pass
+
+# class Dog(Cat):
+#     def speak(self):
+#         print("My DOG NAME IS", self.name)
+
+class Dog(Cat):
+    def speak(self):
+        return super().speak()
+
+chilly = Cat("Chilly")
+shiro = Dog("Shiro")
+
+chilly.speak()
+shiro.speak()
