d7db3fd89508f1fb5e7b5fa1cc899640ba7833cc
Vijay <be1newinner@gmail.com>
2025-04-23 13:51:25 +0530

    feat: restructure App component and enhance styling for todo list functionality

--- BATCH_18/react/vite-app/src/App.tsx
+++ BATCH_18/react/vite-app/src/App.tsx
@@ -1,10 +1,48 @@
 import "./App.css"
 
 export default function App() {
+
+  const items = [
+    {
+      id: 1,
+      text: "task 1"
+    },
+    {
+      id: 2,
+      text: "task 2"
+    },
+    {
+      id: 3,
+      text: "task 3"
+    },
+    {
+      id: 4,
+      text: "task 4"
+    }
+  ]
+
+
   return (
-    <div>
-      <h1 id="text">Hello</h1>
-      <p>Hi</p>
+    <div className="todo_container">
+      <h1>TODO LIST</h1>
+      <hr />
+      <input placeholder="add items..." />
+      <button className="black">ADD ITEM</button>
+
+      <div className="content">
+        {items.map((e) => {
+          return (
+            <div key={e.id}>
+              <p>{e.text}</p>
+              <div>
+                <button>Delete</button>
+                <button>Edit</button>
+              </div>
+            </div>
+          )
+        })}
+      </div>
     </div>
+
   )
 }
