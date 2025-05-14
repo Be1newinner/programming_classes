923a39ad6afb7251f49fcdddfd235066a3ef3350
Vijay <be1newinner@gmail.com>
2025-05-14 13:06:17 +0530

    feat: enhance Navbar component to highlight active route using useLocation

--- BATCH_18/react/multi_page/app/components/Navbar.tsx
+++ BATCH_18/react/multi_page/app/components/Navbar.tsx
@@ -1,16 +1,22 @@
-import { Link } from "react-router";
+import { Link, useLocation } from "react-router";
 
 export default function Navbar() {
+  const location = useLocation();
+  console.log(location.pathname);
   return (
     <header className="p-4">
-      <ul className="flex gap-4 *:bg-red-100 *:text-black *:py-2 *:px-4 *:rounded-md *:active:scale-95 *:cursor-pointer">
-        <li>
+      <ul
+        className={
+          "flex gap-4 *:text-black *:py-2 *:px-4 *:rounded-md *:active:scale-95 *:cursor-pointer "
+        }
+      >
+        <li className={location.pathname === "/" ? "bg-red-400" : "bg-red-100"}>
           <Link to="/">Home Page</Link>
         </li>
-        <li>
+        <li className={location.pathname === "/about" ? "bg-red-400" : "bg-red-100"}>
           <Link to="/about">About Page</Link>
         </li>
-        <li>
+        <li className={location.pathname === "/contact" ? "bg-red-400" : "bg-red-100"}>
           <Link to="/contact">Contact Page</Link>
         </li>
       </ul>
