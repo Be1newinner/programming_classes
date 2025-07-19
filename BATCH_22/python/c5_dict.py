76fa95cd1b028defed5df811eeac0793e8a15e1b
Vijay <be1newinner@gmail.com>
2025-07-19 20:07:23 +0530

    Refactor dictionary example and clean up commented code

--- BATCH_22/python/c5_dict.py
+++ BATCH_22/python/c5_dict.py
@@ -2,19 +2,31 @@
 # person = {}
 
 # dictionary with keys
-person = {
-    "name": "Vijay",
-    "age": 25
-}
+person = {"name": "Vijay", "age": 25}
 
 # print(person)
 # print(person["name"])
 # print(person["age"])
 
 # Updating a key value
-person["name"] = "Vijay Kumar"
+# person["name"] = "Vijay Kumar"
 
 # Deleting a key
-del person["age"]
+# del person["age"]
 
-print(person)
+# print(person["age2"])
+# print(person.get("age2", 0))
+# print("age2" in person)
+# print("age" in person)
+
+# print(len(person))
+
+# person.clear()
+
+# person2 = person.copy()
+
+# print(person2)
+
+# print(person.keys())
+# print(person.values())
+# print(person.items())
