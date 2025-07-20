0f149eb911f06598a0d7a2fddf62fa31f6decb35
Vijay <be1newinner@gmail.com>
2025-07-20 12:21:00 +0530

    set and tuple

---
+++ BATCH_22/python/c6_set.py
@@ -0,0 +1,13 @@
+names = {"Vijay", "Ajay", "Karan", "Arjun", "Sanjay"}
+
+# names.append("Vijay")
+# names.append("Vijay")
+
+names.add("Vijay")
+names.add("Vijay")
+names.add("Vikas")
+
+# names.remove("vikas")
+# names.discard("vikas")
+
+print(names)
---
+++ BATCH_22/python/c6_tuple.py
@@ -0,0 +1,11 @@
+tup = ("Vijay", "Ajay", "Karan", "Arjun", "Sanjay")
+# tup = ("Vijay",)
+nested_tuple = ((1, 2), (3, 4))
+
+# add_tuple = tup + nested_tuple
+mul_tuple = tup * 4
+
+# print(tup)
+# print(nested_tuple[0][0])
+# print(add_tuple)
+print(mul_tuple)
