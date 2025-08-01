fa82afc3bb6a198615745f684723f9198707b863
Vijay <be1newinner@gmail.com>
2025-08-01 17:19:39 +0530

    React Todo class 2

--- BATCH_20/react/demo_project/src/components/Todo.jsx
+++ BATCH_20/react/demo_project/src/components/Todo.jsx
@@ -1,8 +1,11 @@
-import React from "react";
 import { Trash2, SquarePen } from "lucide-react";
+import { useEffect, useState } from "react";
+// import { v4 as uuid } from "uuid"; // For uuid package
 
 export default function Todo() {
-  const todos = [
+  const [inputValue, setInputValue] = useState("");
+
+  const [todos, setTodos] = useState([
     {
       id: 1,
       text: "i will woke up someday 1!",
@@ -27,19 +30,65 @@ export default function Todo() {
       id: 6,
       text: "i will woke up someday 6!",
     },
-  ];
+  ]);
+
+  function addTodo() {
+    // method 1
+    // const temp_todo = [
+    //   ...todos,
+    //   {
+    //     id: 6,
+    //     text: inputValue,
+    //   },
+    // ];
+    // setTodos(temp_todo);
+
+    // method 2
+    // temp_todo.push({
+    //   id: 6,
+    //   text: inputValue,
+    // });
+
+    // method 3
+    // setTodos([
+    //   ...todos,
+    //   {
+    //     id: 6,
+    //     text: inputValue,
+    //   },
+    // ]);
+
+    // method 4
+    setTodos((prev) => [
+      ...prev,
+      {
+        // id: uuid(),
+        id: Date.now(),
+        text: inputValue,
+      },
+    ]);
+  }
+
+  useEffect(() => {
+    console.table(todos);
+  }, [todos]);
 
   return (
     <div className="container">
+      {/* {inputValue} */}
       <div>
-        <input placeholder="enter title!" />
-        <button>ADD</button>
+        <input
+          placeholder="enter title!"
+          value={inputValue}
+          onChange={(e) => setInputValue(e.target.value)}
+        />
+        <button onClick={addTodo}>ADD</button>
       </div>
 
       <div>
         {todos.map((todo) => {
           return (
-            <div>
+            <div key={todo.id}>
               <span>{todo.text}</span>
               <div>
                 <div>
