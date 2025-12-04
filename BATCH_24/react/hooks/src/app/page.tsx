bb380d66d732c0cd093b659253437fbec47d4d75
Vijay <be1newinner@gmail.com>
2025-12-04 16:19:14 +0530

    Added Projects "next Project" and create a card using tailwindcss

--- BATCH_24/react/hooks/src/app/page.tsx
+++ BATCH_24/react/hooks/src/app/page.tsx
@@ -1,51 +1,41 @@
 "use client";
-
 import { useState } from "react";
 
-export default function HomePage() {
-  // let count = 5;
-
-  const [count, setCount] = useState(2);
-  // Rule to know when to create a variable using use state
-  // jab bhi koi variable update hoga and uska update screen pe show krna hoga
-  // use state ki value ko screen pe show krne k liye array ka first part use hoga e.g. count
-  // use state ki value ko update krne k liye sirf array k andar k second part mtlb function ko use krna hai e.g. setCount
+export default function CounterApp() {
+  const [counter, setCounter] = useState(0);
 
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main>
-        <h1 id="_counter">Counter : {count}</h1>
-        <button
-          onClick={() => {
-            // count++;
-            // document.getElementById("_counter").innerHTML = count;
+    <div>
+      <h1>Counter App</h1>
 
-            setCount(count + 1);
-            console.log(count);
-          }}
-        >
-          Increase
-        </button>
-        <br />
-        <button
-          onClick={() => {
-            // count--;
-            setCount(count - 1);
-            console.log(count);
-          }}
-        >
-          Decrease
-        </button>
-      </main>
+      <p>Counter : {counter}</p>
+
+      <button
+        onClick={() => {
+          setCounter(counter + 1);
+        }}
+      >
+        Increase
+      </button>
+      <br />
+      <button
+        onClick={() => {
+          setCounter(counter - 1);
+        }}
+      >
+        Decrease
+      </button>
     </div>
   );
 }
 
 /*
- Cases : this is the pattern to write the name of variables of functions
- types of cases
- 1. PascalCase -> React ka pattern
- 2. snake_case -> python
- 3. camelCase  -> javasscript
- 4. kebab-case
+ Simple Blank Page
+export default function CounterApp() {
+  return (
+    <div>
+      <h1>Counter App</h1>
+    </div>
+  );
+}
 */
---
+++ BATCH_24/react/projects/src/app/page.tsx
@@ -0,0 +1,24 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+      <main className="flex justify-center items-center h-screen">
+        <div className="flex flex-col bg-slate-200 w-80 h-100 rounded-4xl text-black overflow-hidden gap-12">
+          <div className="h-1/2 bg-[url(https://vectordiary.com/wp-content/uploads/2013/12/magic-waves.jpg)] bg-cover">
+
+          </div>
+          <div className="flex flex-col flex-1 justify-evenly pb-4">
+            <h1 className="text-center text-2xl">Vijay Kumar</h1>
+            <p className="text-center text-sm text-gray-600">Full Stack Developer</p>
+            <div className="flex gap-2 justify-center">
+            <button className="btn-outline">View</button>
+            <button className="btn-filled">Contact</button>
+            </div>
+          </div>
+
+        {/* Profile Photo  */}
+          <Image  className="rounded-full h-24 w-24 absolute top-1/2 left-1/2 -translate-1/2 border-10 border-slate-200 hover:rotate-y-[180deg] transition-all duration-[0.5s] cursor-pointer active:scale-[0.9]" src="/images/me.webp" height={150} width={150} alt="DP" />
+        </div>
+      </main>
+  );
+}
