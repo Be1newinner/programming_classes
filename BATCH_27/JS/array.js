0446813ef7ca8e429063b2b26d7aad09aebf5c66
Vijay <be1newinner@gmail.com>
2025-12-15 14:37:30 +0530

    feat: Add initial JavaScript files for array and object examples

---
+++ BATCH_27/JS/array.js
@@ -0,0 +1,41 @@
+// 
+
+// const person = {
+//     name, age, class, subjects, marks
+// }
+
+const names = ["Vijay", "Ajay", "karan", "arjun"]
+const marks = [25,15,16,19,18,15,12]
+const marks2 = []
+
+// console.log(names)
+// console.log(names[0])
+// console.log(names[1] , marks[1] )
+
+// Update
+// names[1] = "Ajay Kumar"
+// console.log(names[1] , marks[1] )
+
+// Data add krne k liye last se
+// marks2.push(15)
+// marks2.push(17)
+// marks2.push(19)
+
+// console.log(marks2)
+
+// Data delete krne k liye last se
+// marks.pop()
+// marks.pop()
+// marks.pop()
+// console.log(marks)
+
+
+// Data add krne k liye start se
+// marks.unshift(1)
+// marks.unshift(2)
+
+// Data delete krne k liye start se
+
+marks.shift()
+marks.shift()
+console.log(marks)
---
+++ BATCH_27/JS/objects.js
@@ -0,0 +1,25 @@
+// const name = "Vijay";
+// const age = 25;
+// const roll = 1;
+// const name2 = "Ajay";
+// const age2 = 26;
+// const roll2 = 2;
+
+
+// Objects: It groups all the data related to one person in a single variable
+
+const person = {
+    name: "Ajay",
+    age: 26,
+    roll: 2
+}
+
+
+const person1 = {
+  name: "Vijay",
+  age: 25,
+  roll: 1,
+};
+
+
+console.log(person)
