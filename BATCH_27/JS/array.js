865126f34b038d4c269aa349a3d2ec623ebd0860
Vijay <be1newinner@gmail.com>
2025-12-17 13:03:01 +0530

    refactor: Clean up array.js by organizing comments and formatting

--- BATCH_27/JS/array.js
+++ BATCH_27/JS/array.js
@@ -1,12 +1,11 @@
-// 
+//
 
 // const person = {
 //     name, age, class, subjects, marks
 // }
 
-const names = ["Vijay", "Ajay", "karan", "arjun"]
-const marks = [25,15,16,19,18,15,12]
-const marks2 = []
+const names = ["Vijay", "Ajay", "karan", "arjun"];
+const marks = [25, 15, 16, 19, 18, 15, 12];
 
 // console.log(names)
 // console.log(names[0])
@@ -29,13 +28,45 @@ const marks2 = []
 // marks.pop()
 // console.log(marks)
 
-
 // Data add krne k liye start se
 // marks.unshift(1)
 // marks.unshift(2)
 
 // Data delete krne k liye start se
+// marks.shift()
+// marks.shift()
 
-marks.shift()
-marks.shift()
-console.log(marks)
+
+// Data add ya remove krne k liye mid se
+// marks.splice(2, 3);
+// marks.splice(2, 0, 78, 85);
+// marks.splice(2, 1, 78);
+
+// Shallow Copy / copy by reference
+// const marks2 = marks;
+
+// Deep Copy / Copy by value
+// const marks2 = marks.slice(); // slice method makes a clone of array
+// marks.push(1)
+
+// console.log(marks);
+// console.log(marks2);
+// console.log(marks.slice(1,4));
+// console.log(marks.slice(2,6));
+
+
+// find the number of items in array
+// console.log(marks.length)
+
+
+// find the item in the array
+// 
+// includes method = true or false
+// console.log(marks.includes(19))
+// console.log(marks.includes(199))
+
+
+// indexOf method
+// console.log(marks.indexOf(15))
+// console.log(marks.lastIndexOf(15))
+
